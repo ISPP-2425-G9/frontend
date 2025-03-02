c1ab1d30c90a3018ba73289711ae49558ebfcebb
rafduqcol <rafduqcol@alum.us.es>
2025-03-02 14:10:23 +0100

    fix: Change navitgation os createObituary

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -89,6 +89,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen name="obituaries/index" options={{ title: "Esquelas" }} />
+      <Tabs.Screen name="obituaries/createObituary" options={{ href: null }} />
       <Tabs.Screen name="messages/index" options={{ title: "Mensajes" }} />
       <Tabs.Screen name="contacts/index" options={{ title: "Contactos" }} />
       <Tabs.Screen name="services/index" options={{ title: "Servicios" }} />
