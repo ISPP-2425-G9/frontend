ca65b091e0c84af740eb7ddd6102fb4e47eee378
dangalcan <megamagolas@gmail.com>
2025-03-06 12:27:46 +0100

    feat: include Backend API url enviroment variable use

--- app/register/index.tsx
+++ app/register/index.tsx
@@ -6,6 +6,8 @@ import CustomModal from '@/components/CustomModal';
 import TextInputArraysForm from '@/components/TextInputArraysForm';
 import { GlobalStyles } from '@/constants/Colors';
 import { InputField } from '@/components/TextInputArraysForm';
+import { BACKEND_API } from '@env';
+
 
 const { width } = Dimensions.get('window');
 
@@ -107,7 +109,7 @@ const RegisterScreen: React.FC = () => {
         throw new Error(`Hay error(es) en su formulario: ${errors}`)
       }
       const reqUrl = userType == 'Empresa' ? 'api/auth/companies/signup' : 'api/auth/customers/signup';
-      const response = await fetch(reqUrl, {
+      const response = await fetch(`${BACKEND_API}/${reqUrl}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
