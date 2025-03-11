11b02788a2f50d1c8f6600e5e429d8059e501722
jormunrod <jorgemr@protonmail.ch>
2025-03-11 01:05:54 +0100

    feat: update user type selection button to show alert for unavailable feature

--- app/register/index.tsx
+++ app/register/index.tsx
@@ -165,7 +165,7 @@ const RegisterScreen: React.FC = () => {
         style={styles.modalStyle}
       >
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={() => handleUserTypeSelection('Empresa')}>
+          <TouchableOpacity style={styles.button} onPress={() => alert('Esta función no está disponible aún')}>
             <Text style={styles.buttonText}>Empresa</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.button} onPress={() => handleUserTypeSelection('Cliente')}>
