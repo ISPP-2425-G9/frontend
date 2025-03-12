fb6aec798c4c08e97feba102045902eff1e3ccfc
hugborang <hugo.bo.an@gmail.com>
2025-03-12 14:26:16 +0100

    feat: responsive design for obituaries creation (not finished)

--- app/obituaries/createObituary.tsx
+++ app/obituaries/createObituary.tsx
@@ -11,6 +11,7 @@ import {
   Alert,
   Platform,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import CustomButton from "@/components/CustomButton";
@@ -26,6 +27,7 @@ import { GlobalStyles } from "@/constants/Colors";
 import { BACKEND_API } from "@/constants/Mysc";
 import { withAuth } from "../_util/withAuth";
 import { AUTHORITIES } from "../_util/Authorities";
+import { green } from "react-native-reanimated/lib/typescript/Colors";
 
 const width = Dimensions.get("window").width;
 const height = Dimensions.get("window").height;
@@ -237,188 +239,191 @@ function EsquelaCustomizer() {
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.formSection}>
-        <Text style={{ fontSize: 30, fontWeight: "bold", marginBottom: 40 }}>
-          {is_newObituary ? "Crea tu esquela" : "Edita tu esquela"}
-        </Text>
-        <Text>Nombre del fallecido:</Text>
-        <CustomTextInput
-          style={{ width: "75%" }}
-          placeholder="Nombre"
-          maxLength={37}
-          value={formData.name}
-          onChangeText={(text) => handleChange("name", text)}
-        />
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <View style={styles.container}>
+        <View style={styles.formSection}>
+          <Text style={{ fontSize: 30, fontWeight: "bold", marginBottom: 30 }}>
+            {is_newObituary ? "Crea tu esquela" : "Edita tu esquela"}
+          </Text>
+          <Text>Nombre del fallecido:</Text>
+          <CustomTextInput
+            style={{ width: "75%" }}
+            placeholder="Nombre"
+            maxLength={37}
+            value={formData.name}
+            onChangeText={(text) => handleChange("name", text)}
+          />
 
-        <Text>Fecha de nacimiento:</Text>
-        <CustomTextInput
-          style={{ width: "75%" }}
-          placeholder="dd/mm/aaaa"
-          value={formData.birthDate}
-          maxLength={10}
-          keyboardType="numeric"
-          onChangeText={(text) => {
-            let cleaned = text.replace(/\D/g, "");
-            let day = "";
-            let month = "";
-            let year = "";
-            if (cleaned.length >= 1) day = cleaned.slice(0, 2);
-            if (cleaned.length >= 3) month = cleaned.slice(2, 4);
-            if (cleaned.length >= 5) year = cleaned.slice(4, 8);
-            if (day.length === 2) {
-              let dayNum = parseInt(day, 10);
-              if (dayNum > 31) day = "31";
-              else if (dayNum < 1) day = "01";
-              else day = dayNum.toString().padStart(2, "0");
-            }
-            if (month.length === 2) {
-              let monthNum = parseInt(month, 10);
-              if (monthNum > 12) month = "12";
-              else if (monthNum < 1) month = "01";
-              else month = monthNum.toString().padStart(2, "0");
-            }
-            if (year.length === 4) {
-              let yearNum = parseInt(year, 10);
-              if (yearNum < 1900) year = "1900";
-              else if (yearNum > 2025) year = "2025";
-              else year = yearNum.toString();
-            }
-            let formatted = day;
-            if (month) formatted += "/" + month;
-            if (year) formatted += "/" + year;
-            if (formatted.length > 10) formatted = formatted.slice(0, 10);
-
-            handleChange("birthDate", formatted);
-          }}
-        />
-
-        <Text>Fecha de fallecimiento:</Text>
-        <CustomTextInput
-          style={{ width: "75%" }}
-          placeholder="La fecha de fallecimiento se llenará automáticamente"
-          value={formData.deathDate}
-          maxLength={12}
-          editable={false}
-          onChangeText={(text) => handleChange("deathDate", text)}
-          keyboardType="numeric"
-        />
-
-        <Text>Mensaje de despedida:</Text>
-        <CustomTextInput
-          style={[styles.textArea, { width: "75%" }]}
-          placeholder="Escribe un mensaje de despedida"
-          maxLength={624}
-          multiline
-          value={formData.farewellMessage}
-          onChangeText={(text) => handleChange("farewellMessage", text)}
-        />
-
-        <Text>Frase de despedida:</Text>
-        <CustomTextInput
-          style={{ width: "75%" }}
-          placeholder="Frase de despedida"
-          maxLength={90}
-          value={formData.farewellPhrase}
-          onChangeText={(text) => handleChange("farewellPhrase", text)}
-        />
-
-        {!formData.deathDate && (
-          <>
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-around",
-                gap: 8,
-                width: "75%",
-              }}
-            >
-              <CustomButton
-                style={{ marginTop: 12, width: "49%" }}
-                title="Selecciona una imagen"
-                onPress={pickImage}
-              />
-              <CustomButton
-                style={{ marginTop: 12, width: "49%" }}
-                title="Cambia el diseño de tu esquela"
-                onPress={changeDesign}
-              />
-            </View>
-            <CustomButton
-              color="grey"
-              style={{ marginTop: 12, width: "75%" }}
-              title={is_newObituary ? "Guardar y seleccionar contactos" : "Actualice sus contactos"}
-              onPress={showConfirmationModal}
-            />
-          </>
-        )}
-      </View>
-
-      <View style={styles.previewSection}>
-        <View style={styles.overlayContainer}>
-          <Image source={{ uri: imageUrl }} style={styles.templateImage} />
-          <View style={styles.overlayContent}>
-            <Image
-              source={
-                formData.customImage
-                  ? { uri: formData.customImage }
-                  : require("@/assets/images/default-dark-image.jpg")
+          <Text>Fecha de nacimiento:</Text>
+          <CustomTextInput
+            style={{ width: "75%" }}
+            placeholder="dd/mm/aaaa"
+            value={formData.birthDate}
+            maxLength={10}
+            keyboardType="numeric"
+            onChangeText={(text) => {
+              let cleaned = text.replace(/\D/g, "");
+              let day = "";
+              let month = "";
+              let year = "";
+              if (cleaned.length >= 1) day = cleaned.slice(0, 2);
+              if (cleaned.length >= 3) month = cleaned.slice(2, 4);
+              if (cleaned.length >= 5) year = cleaned.slice(4, 8);
+              if (day.length === 2) {
+                let dayNum = parseInt(day, 10);
+                if (dayNum > 31) day = "31";
+                else if (dayNum < 1) day = "01";
+                else day = dayNum.toString().padStart(2, "0");
               }
-              style={styles.customImage}
-            />
-            <Text style={styles.previewName}>{formData.name || "Nombre "}</Text>
-            <Text style={styles.previewDate}>
-              {formData.birthDate || "Año de nacimiento"} -{" "}
-              {formData.deathDate || "Año de fallecimiento"}
-            </Text>
-            <Text style={styles.previewText}>
-              {formData.farewellMessage ||
-                "Tu mensaje de despedida aparecerá aquí"}
-            </Text>
-            <Text style={styles.previewPhrase}>
-              "{formData.farewellPhrase || "Frase de despedida"}"
-            </Text>
+              if (month.length === 2) {
+                let monthNum = parseInt(month, 10);
+                if (monthNum > 12) month = "12";
+                else if (monthNum < 1) month = "01";
+                else month = monthNum.toString().padStart(2, "0");
+              }
+              if (year.length === 4) {
+                let yearNum = parseInt(year, 10);
+                if (yearNum < 1900) year = "1900";
+                else if (yearNum > 2025) year = "2025";
+                else year = yearNum.toString();
+              }
+              let formatted = day;
+              if (month) formatted += "/" + month;
+              if (year) formatted += "/" + year;
+              if (formatted.length > 10) formatted = formatted.slice(0, 10);
+
+              handleChange("birthDate", formatted);
+            }}
+          />
+
+          <Text>Fecha de fallecimiento:</Text>
+          <CustomTextInput
+            style={{ width: "75%" }}
+            placeholder="La fecha de fallecimiento se añadirá automáticamente"
+            value={formData.deathDate}
+            maxLength={12}
+            editable={false}
+            onChangeText={(text) => handleChange("deathDate", text)}
+            keyboardType="numeric"
+          />
+
+          <Text>Mensaje de despedida:</Text>
+          <CustomTextInput
+            style={[{ width: "75%" }]}
+            placeholder="Escribe un mensaje de despedida"
+            value={formData.farewellMessage}
+            maxLength={624}
+            //multiline
+            onChangeText={(text) => handleChange("farewellMessage", text)}
+          />
+
+          <Text>Frase de despedida:</Text>
+          <CustomTextInput
+            style={{ width: "75%" }}
+            placeholder="Frase de despedida"
+            maxLength={90}
+            value={formData.farewellPhrase}
+            onChangeText={(text) => handleChange("farewellPhrase", text)}
+          />
+
+          {!formData.deathDate && (
+            <>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-around",
+                  gap: 8,
+                  width: "75%",
+                }}
+              >
+                <CustomButton
+                  style={{ marginTop: 12, width: "49%" }}
+                  title="Selecciona una imagen"
+                  onPress={pickImage}
+                />
+                <CustomButton
+                  style={{ marginTop: 12, width: "49%" }}
+                  title="Cambia el diseño de tu esquela"
+                  onPress={changeDesign}
+                />
+              </View>
+              <CustomButton
+                color="grey"
+                style={{ marginTop: 12, width: "75%" }}
+                title={is_newObituary ? "Guardar y seleccionar contactos" : "Actualice sus contactos"}
+                onPress={showConfirmationModal}
+              />
+            </>
+          )}
+        </View>
+
+        <View style={styles.previewSection}>
+          <View style={styles.overlayContainer}>
+            <Image source={{ uri: imageUrl }} style={styles.templateImage} />
+            <View style={styles.overlayContent}>
+              <Image
+                source={
+                  formData.customImage
+                    ? { uri: formData.customImage }
+                    : require("@/assets/images/default-dark-image.jpg")
+                }
+                style={styles.customImage}
+              />
+              <Text style={styles.previewName}>{formData.name || "Nombre "}</Text>
+              <Text style={styles.previewDate}>
+                {formData.birthDate || "Año de nacimiento"} -{" "}
+                {formData.deathDate || "Año de fallecimiento"}
+              </Text>
+              <Text style={styles.previewText}>
+                {formData.farewellMessage ||
+                  "Tu mensaje de despedida aparecerá aquí"}
+              </Text>
+              <Text style={styles.previewPhrase}>
+                "{formData.farewellPhrase || "Frase de despedida"}"
+              </Text>
+            </View>
           </View>
         </View>
+        {modalVisible && (
+          <CustomModal
+            visible={modalVisible}
+            onClose={handleCloseModal}
+            title={modalMessage}
+            style={styles.modalStyle}
+          >
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleSubmit()}
+              >
+                <Text style={styles.buttonText}>Aceptar</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleCloseModal()}
+              >
+                <Text style={styles.buttonText}>Cancelar</Text>
+              </TouchableOpacity>
+            </View>
+          </CustomModal>
+        )}
       </View>
-      {modalVisible && (
-        <CustomModal
-          visible={modalVisible}
-          onClose={handleCloseModal}
-          title={modalMessage}
-          style={styles.modalStyle}
-        >
-          <View style={styles.buttonContainer}>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => handleSubmit()}
-            >
-              <Text style={styles.buttonText}>Aceptar</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => handleCloseModal()}
-            >
-              <Text style={styles.buttonText}>Cancelar</Text>
-            </TouchableOpacity>
-          </View>
-        </CustomModal>
-      )}
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
-    padding: 16,
-    paddingTop: 110,
+    flexDirection: width > 600 ? "row" : "column",
+    paddingTop: 105,
   },
   formSection: {
     flex: 1,
-    paddingLeft: 100,
-    alignItems: "flex-start",
+    paddingLeft: width > 600 ? 80 : 0, 
+    alignItems: width > 600 ? "flex-start" : "center",
+    width: "100%",
+    gap: "1%",
   },
   modalStyle: {
     backgroundColor: "#fff",
@@ -436,19 +441,19 @@ const styles = StyleSheet.create({
     position: "relative",
   },
   previewText: {
-    fontSize: 15,
-    maxWidth: 400,
+    fontSize: width > 600 ? 16 : 12,
+    maxWidth: width > 600 ? 400 : "62%",
     marginTop: 8,
     textAlign: "justify",
   },
   previewName: {
-    fontSize: 20,
+    fontSize: width > 600 ? 20 : 16,
     fontWeight: "bold",
     marginTop: 8,
     maxWidth: 400,
   },
   previewDate: {
-    fontSize: 18,
+    fontSize: width > 600 ? 18 : 14,
     marginBottom: 8,
     maxWidth: 400,
     marginTop: 8,
@@ -462,24 +467,17 @@ const styles = StyleSheet.create({
     gap: "2%",
   },
   previewPhrase: {
-    marginTop: 20,
-    fontSize: 16,
+    marginTop: 15,
+    fontSize: width > 600 ? 16 : 13,
     fontStyle: "italic",
-    maxWidth: 400,
+    maxWidth: width > 600 ? 400 : "62%",
     justifyContent: "center",
     textAlign: "center",
-  },
-  textArea: {
-    height: 160,
-  },
-  previewTitle: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginBottom: 12,
   },
   overlayContainer: {
     position: "relative",
     alignItems: "center",
+    //backgroundColor: "#ff0000", 
   },
   templateImage: {
     width: width * 0.83,
@@ -490,12 +488,14 @@ const styles = StyleSheet.create({
     position: "absolute",
     alignItems: "center",
     marginTop: 100,
+    //backgroundColor: "#00ff00",
   },
   customImage: {
-    width: 100,
-    height: 100,
+    width: width > 600 ? 100 : 60,
+    height: width > 600 ? 100 : 60,
     borderRadius: 50,
-    marginBottom: 8,
+    marginBottom: 6,
+    marginTop: width > 600 ? 0 : "0%",
   },
   button: {
     backgroundColor: GlobalStyles.blue,
