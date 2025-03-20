30b8413319214299f1269a5e9296b8eeb759fb69
Karyouben <karyouben@alum.us.es>
2025-03-20 18:07:44 +0100

    fix: unused error planCard

--- components/PlanCard.tsx
+++ components/PlanCard.tsx
@@ -222,7 +222,6 @@ const PlanCard: React.FC<PlanCardProps> = ({ role, fechaExpiracion }) => {
         planType={isCustomer ? 'CUSTOMER_PREMIUM' : 'COMPANY_PREMIUM'}
         description={`Suscripción al ${isCustomer ? 'plan mensual - Mensajes de despedida' : 'Plan Premium - Publicita tu Empresa'}`}
         onSuccess={handlePaymentSuccess}
-        onError={handlePaymentError}
       />
     </View>
   );
