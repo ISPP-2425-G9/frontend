5e6f2511cfaf93798a8aa975f6c21d7b15532de1
dangalcan <megamagolas@gmail.com>
2025-03-31 20:26:14 +0200

    test: add some ui default components tests

---
+++ components/__tests__/TabBarBackground.ios-test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import BlurTabBarBackground, { useBottomTabOverflow } from '../ui/TabBarBackground.ios';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+describe('BlurTabBarBackground Component', () => {
+  it('renders correctly with blur effect', () => {
+    const { toJSON } = render(
+      <SafeAreaProvider>
+        <BlurTabBarBackground />
+      </SafeAreaProvider>
+    );
+    expect(toJSON()).toMatchSnapshot();
+  });
+});
+
