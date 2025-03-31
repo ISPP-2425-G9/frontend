05e2748f28a51ca2522698eddad5f7b51a9098fc
dangalcan <megamagolas@gmail.com>
2025-03-31 20:37:12 +0200

    fix: fix codacy problem in .sh file again

--- scripts/installTestingDependencies.sh
+++ scripts/installTestingDependencies.sh
@@ -1,4 +1,4 @@
-# bin/bash
+#!/bin/sh
 ## In case you are having problems with npm test, try running this
 npx expo install @testing-library/react-native "--" --dev
 npm install -D eslint-plugin-testing-library
