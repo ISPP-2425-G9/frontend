539a024b0f81973a8ddeca327559cc8e786715d1
dangalcan <megamagolas@gmail.com>
2025-03-31 20:35:36 +0200

    fix: add bin specs to fix Codacy

--- scripts/installTestingDependencies.sh
+++ scripts/installTestingDependencies.sh
@@ -1,3 +1,4 @@
+# bin/bash
 ## In case you are having problems with npm test, try running this
 npx expo install @testing-library/react-native "--" --dev
 npm install -D eslint-plugin-testing-library
