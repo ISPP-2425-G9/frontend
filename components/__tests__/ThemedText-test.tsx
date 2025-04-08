5125a5ccaf8704157302066803daee8becb9be61
Karyouben <karyouben@alum.us.es>
2025-04-09 00:54:25 +0200

    test: improved imageWithText-test

--- components/__tests__/ThemedText-test.tsx
+++ components/__tests__/ThemedText-test.tsx
@@ -4,7 +4,6 @@ import { render } from '@testing-library/react-native';
 
 import { ThemedText } from '../ThemedText';
 
-// Mock GlobalStyles
 jest.mock('@/constants/Colors', () => ({
   GlobalStyles: {
     font: 'Arial',
