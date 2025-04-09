096fed2e5322661d2799ee5147cd7ce1d8a67e36
dangalcan <megamagolas@gmail.com>
2025-04-09 17:19:46 +0200

    test: improve subscribe test

--- components/__tests__/Subscribe-test.tsx
+++ components/__tests__/Subscribe-test.tsx
@@ -7,17 +7,19 @@ import useAuth from '@/hooks/useAuth';
 import { NavigationContainer } from '@react-navigation/native';
 import { NotificationProvider } from '@/context/NotificationContext';
 
-// Mock de useAuth
 jest.mock('@/hooks/useAuth');
-jest.mock('@/components/PlanCard', () => 'PlanCard'); // Mock de PlanCard
+
+const MockedPlanCard = jest.fn(() => null);
+
+jest.mock('@/components/PlanCard', () => MockedPlanCard);
 
 jest.mock('@react-native-async-storage/async-storage', () => ({
-    getItem: jest.fn(),
-    setItem: jest.fn(),
-    removeItem: jest.fn(),
-    clear: jest.fn(),
-    getAllKeys: jest.fn(),
-  }));
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+  getAllKeys: jest.fn(),
+}));
 
 describe('PlanManagementView', () => {
 
@@ -28,10 +30,10 @@ describe('PlanManagementView', () => {
       </NavigationContainer>
     );
   };
-  
+
   beforeEach(() => {
-    // Restablecemos el mock de useAuth antes de cada prueba
     useAuth.mockReset();
+    MockedPlanCard.mockClear();
   });
 
   it('should render loading state when role is not defined', () => {
@@ -39,9 +41,32 @@ describe('PlanManagementView', () => {
       roles: null,
       expiredPlanDate: null,
     });
+
     const { getByTestId } = renderWithNavigation(<PlanManagementView />);
-    // Verificar que el texto de carga se muestra
-    expect(screen.getByTestId('loading-indicator')).toBeTruthy();
+    expect(getByTestId('loading-indicator')).toBeTruthy();
+  });
+
+  it('should not render loading state if roles are available', async () => {
+    useAuth.mockReturnValue({
+      roles: ['CUSTOMER_FREE'],
+      expiredPlanDate: null,
+    });
+
+    renderWithNavigation(<PlanManagementView />);
+
+    await waitFor(() => {
+      expect(screen.queryByTestId('loading-indicator')).toBeNull();
+    });
+  });
+
+  it('should render the correct role-based PlanCard props', async () => {
+    const expirationDate = new Date('2025-05-01');
+    useAuth.mockReturnValue({
+      roles: ['COMPANY_PREMIUM'],
+      expiredPlanDate: expirationDate,
+    });
+
+    renderWithNavigation(<PlanManagementView />);
   });
 
 });
